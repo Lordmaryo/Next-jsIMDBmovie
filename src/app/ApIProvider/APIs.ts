e6b1b0aef1d62f09c2a13d4ea709acbe6ea02c75
LordMaryo <emmanuelofoneta@gmail.com>
2024-06-06 12:28:17 +0100

    Modify APIs

--- src/app/ApIProvider/APIs.ts
+++ src/app/ApIProvider/APIs.ts
@@ -1,45 +1,30 @@
 export const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
-// TODO: create 4 api urls and display to homePage 
-
 interface DataProps {
     id: number;
     original_title: string;
     overview: string;
 }
 
-// Trending data url
+
+async function fetchData(endpoint: string): Promise<DataProps[]> {
+    const result = await fetch(`https://api.themoviedb.org/3/${endpoint}?api_key=${API_KEY}`);
+    const data = await result.json();
+    return data.results as DataProps[];
+}
+
 export async function fetchTrendingData(): Promise<DataProps[]> {
-    const result = await fetch(
-        `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}`
-    );
-    const data = await result.json();
-    return data.results as DataProps[];
+    return fetchData('trending/all/week');
 }
 
-//Top rated data url
 export async function topRated(): Promise<DataProps[]> {
-    const result = await fetch(
-        `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`
-    );
-    const data = await result.json();
-    return data.results as DataProps[];
+    return fetchData('movie/top_rated');
 }
 
-// upcoming movies data url
 export async function upComingMovies(): Promise<DataProps[]> {
-    const result = await fetch(
-        `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}`
-    );
-    const data = await result.json();
-    return data.results as DataProps[];
+    return fetchData('movie/upcoming');
 }
 
-//fan favorites
 export async function fanFavorites(): Promise<DataProps[]> {
-    const result = await fetch(
-        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-    );
-    const data = await result.json();
-    return data.results as DataProps[];
+    return fetchData('movie/popular');
 }
